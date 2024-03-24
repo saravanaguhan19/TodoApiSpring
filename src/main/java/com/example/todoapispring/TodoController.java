c215ff213cf65d0d76767c2450890214e8578b6d
saravanaguhan19 <saravanaguhan19@gmail.com>
2024-03-24 11:39:23 +0530

    response code added

--- src/main/java/com/example/todoapispring/TodoController.java
+++ src/main/java/com/example/todoapispring/TodoController.java
@@ -1,9 +1,8 @@
 package com.example.todoapispring;
 
-import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.PostMapping;
-import org.springframework.web.bind.annotation.RequestBody;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.*;
 
 import java.util.ArrayList;
 import java.util.List;
@@ -11,27 +10,40 @@ import java.util.List;
 @RestController
 public class TodoController {
 
-    public static List<Todo> todos;
+    public static List<Todo> todoList;
+    public static final  String TODO_NOT_FOUND = "todo not found";
 
     public TodoController (){
-        this.todos = new ArrayList<>();
-        todos.add(new Todo(1,false ,"this is dummy 1 todo" ,231 ));
-        todos.add(new Todo(2,true ,"this is dummy 2 todo" ,232 ));
+        this.todoList = new ArrayList<>();
+        todoList.add(new Todo(1,false ,"this is dummy 1 todo" ,231 ));
+        todoList.add(new Todo(2,true ,"this is dummy 2 todo" ,232 ));
     }
     @GetMapping("/todos")
+    @ResponseStatus(HttpStatus.ACCEPTED)
     public List<Todo> getTodos(){
-        return todos;
+        return todoList;
     }
 
     @PostMapping ("/todos")
-    public Todo createTodo (@RequestBody Todo newTodo){
 
-        todos.add(newTodo);
+    public ResponseEntity<Todo> createTodo (@RequestBody Todo newTodo){
 
-        return newTodo;
+        todoList.add(newTodo);
+
+        return ResponseEntity.status(HttpStatus.CREATED).body(newTodo);
 
     }
-
+    @GetMapping("/todos/{todoId}")
+    public ResponseEntity  <?> getTodoById( @PathVariable long todoId){
+        for (Todo todo: todoList) {
+            if(todo.getId() == todoId){
+                return ResponseEntity.ok(todo);
+            }
+            
+        }
+        return ResponseEntity.
+        status(HttpStatus.NOT_FOUND).body(TODO_NOT_FOUND);
+    }
 
 
 
